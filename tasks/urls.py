82286cf9c34ffc684f235fd62f65e6f9c0b31a50
Hemant Thanna <Hemantthanna10@gmail.com>
2023-05-16 23:44:05 +0530

    first commit

---
+++ tasks/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='list'),
+    path('update_task/<str:primarykey>/', views.updateTask, name="update_task"),
+    path('delete_task/<str:primarykey>/', views.deleteTask, name="delete_task")
+]
